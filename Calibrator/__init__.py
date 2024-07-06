55cd88fe7887e7e1774b60a29b4156c4616fd73d
Nick Häcker <nick.athaeck@googlemail.com>
2024-07-06 21:37:40 +0200

    create file

--- Calibrator/__init__.py
+++ Calibrator/__init__.py
@@ -2,15 +2,7 @@ import asyncio
 import websockets
 from pyee import AsyncIOEventEmitter
 import json
-import argparse
-import os
-# from CamParamsExtractor import CamParamsExtractor
-# from ExtrinsicCalculator import ExtrinsicCalculator
-# from CamExtris import CamExtris
-# from ExtriJsonHandlers import ExtriJsonCreator, ExtriJsonLoader
-# from ComKinectRecordingExtractor import ComKinectRecordingExtractor
-# from PcdHandler import PcdHandler
-import time
+from calibrating.src.BaseCalibrator import BaseCalibrator
 
 
 webSocketAdress: str = "192.168.178.34"
@@ -48,136 +40,51 @@ def CreateStateUpdate(state:str):
     stateUpdate.addData("State",state)
     return stateUpdate.JSONString
 
-# class Calibrator:
 
-#     def get_parsed_args(self):
-#         parser = argparse.ArgumentParser(description="Code to Extract Pointclouds an Extrinsic Params of Cams.")
-#         # parser.add_argument("--recordings_path", type=str, help="Path to all Recordings")
-#         # parser.add_argument("--mkvToolNix_path", type=str, help="Path to installed MKVToolNix")
-#         # parser.add_argument("--amount_Subs", type=int, help="e.g. If you have 3 Cams, you have 2 Subs")
-#         # parser.add_argument("--marker_length", type=float, help="length of the Marker...they should be square")        
-#         # # parser.add_argument("--sub_path", type=str, default="", help="Folder name of Scene")       
-#         #  # parser.add_argument("--create_pcd_json", action='store_true', help="Flag if an pcd Json of multiple Frames should be created")
+class SocketCalibrator(BaseCalibrator):
 
-#         parser.add_argument("--use_charuco", action='store_true', help="Flag to set if the calibration Target was a charuco board")
-#         # parser.add_argument("--kinect_pic_rec_extractor", default=".\\venv\\Lib\\site-packages\\open3d\\examples\\reconstruction_system\\sensors", type=str)
-#         parser.add_argument("--icp_itteration", type=int, default=25, help="Amount of ICP Itterations for the Pointcloud Postpro")
-#         parser.add_argument("--amount_pcd_frames", type=int, default=5, help="Amount of Frames which should be saved in pcd json. If --create_pcd is False, this is not necessary.")
-#         parser.add_argument("--create_dg_init_npz", action='store_true', help="If you want to create a dynamic Gaussian, you need a init pcd an you should set this to yes")
-#         parser.add_argument("--pcd_just_center", action='store_true', help="If the whole set should be shown or just the center")
-#         parser.add_argument("--create_npzs", action='store_true', help="If you want to use dynamic gaussian spaltting you may need some npzs...this could become very disc intense (maybe 10GB or more)")
-#         return parser.parse_args()
+    async def SetStatus(self, status):
 
-#     def ensure_trailing_backslash(self,path):
-#             if not path.endswith("\\"):
-#                 return path + "\\"
-#             return path
+        print(status)
+        await self.webSocket.send(CreateStateUpdate(status))
 
-#     def get_all_extrinsic_Matrices(self,recording_path, marker_length, amount_subs, use_charuco, ch_marker_len = 0.112, ch_square_len = 0.150, ch_dict = 0, ch_board_size=(3,5)):
-#         all_extris = []
-#         if not os.path.isfile(recording_path + "extris.json"):
-#             print("Extrinsic Json doesn't exist. Creating new one")
-#             all_extris = []
-#             trackerM = ExtrinsicCalculator(
-#                 path=recording_path, 
-#                 marker_length=marker_length, 
-#                 cam_role="M", 
-#                 use_Charuco=use_charuco, 
-#                 ch_marker_len=ch_marker_len, 
-#                 ch_square_len=ch_square_len,
-#                 ch_dict=ch_dict,
-#                 ch_board_size=ch_board_size
-#                 )
-#             extrM = CamExtris("M", trackerM.get_extrinsic_matrix())
-#             all_extris.append(extrM)
+    def GetProxy(self, baseProxy):
 
-#             for sub in range(amount_subs):
-#                 tracker = ExtrinsicCalculator(
-#                     path=recording_path, 
-#                     marker_length=marker_length, 
-#                     cam_role="S" + str(sub + 1),
-#                     use_Charuco=use_charuco, 
-#                     ch_marker_len=ch_marker_len, 
-#                     ch_square_len=ch_square_len,
-#                     ch_dict=ch_dict,
-#                     ch_board_size=ch_board_size)
-#                 extr = CamExtris("S"+str(sub + 1), tracker.get_extrinsic_matrix())
-#                 all_extris.append(extr)
-#                 print("working: " + str(all_extris))
+        print(baseProxy)
 
-#             ExtriJsonCreator(all_extris, recording_path)
-#         else: 
-#             print("Extrinsic Json exist. Continuing with old values")
-#             extri_loader = ExtriJsonLoader(str(recording_path + "extris.json"))
-#             all_extris = extri_loader.get_all_extris()
-#         return all_extris
+        proxy = {}
 
-#     async def main(self,websocket ,subCount,markerLength,subPath,createPCDJSON,useCharuco,icpItteration,amountPcdFrames,pcdJustCenter,createNpzs,createDgInitNpz=False ):
-#         recordingPath = "C:\\Users\\nicka\\Desktop\\Test"
-#         toolKitPath = "C:\\Program Files\\MKVToolNix"
-#         kinectPicRecExtractor =".\\venv\\Lib\\site-packages\\open3d\\examples\\reconstruction_system\\sensors"
+        proxy.recordings_path = "C:\\Users\\nicka\\Desktop\\Test"
+        proxy.mkvToolNix_path = "C:\\Program Files\\MKVToolNix"
+        proxy.kinect_pic_rec_extractor =".\\venv\\Lib\\site-packages\\open3d\\examples\\reconstruction_system\\sensors"
 
-#         await websocket.send(CreateStateUpdate("Calibrating"))
+        proxy.create_dg_init_npz =""
 
+        proxy.amount_Subs = baseProxy["SubCount"]
+        proxy.marker_length = baseProxy["MarkerLength"]
+        proxy.sub_path= baseProxy["SubPath"]
+        proxy.create_pcd_json = baseProxy["CreateJSON"]
+        proxy.use_charuco = baseProxy["UseCharuco"]
+        proxy.icp_itteration = baseProxy["IcpItteration"]
+        proxy.amount_pcd_frames = baseProxy["AmountPcdFrames"]
+        proxy.pcd_just_center = baseProxy["PcdJustCenter"]
+        proxy.create_npzs = baseProxy["CreateNpzs"]
 
-#         # args = self.get_parsed_args()
-#         recordingPath = self.ensure_trailing_backslash(recordingPath)
-#         toolKitPath = self.ensure_trailing_backslash(toolKitPath)
-#         # Extract Cam Params
-#         CamParamsExtractor(recordingPath, toolKitPath, subCount)
-#         # Get Extrinsic Cam-Params
-#         all_extris = []
-#         all_extris = self.get_all_extrinsic_Matrices(
-#             recording_path=recordingPath, 
-#             marker_length=markerLength, 
-#             amount_subs=subCount, 
-#             use_charuco=useCharuco)
-        
-        
-#         # Create rgb and Depth Images
-#         ComKinectRecordingExtractor(recordingPath, subPath, kinectPicRecExtractor, subCount)
-#         #PCD creation, postpro and visualisation
-#         #Open3d will visualise the pcd of whole Scene but the pcd.json will only be the person in the middle
-#         PcdHandler(
-#             loading_amount=amountPcdFrames, 
-#             sub_amount=subCount, 
-#             icp_its=icpItteration, 
-#             just_show_center=pcdJustCenter, 
-#             path=recordingPath, 
-#             sub_path=subPath, 
-#             extris=all_extris, 
-#             create_pcd_json=createPCDJSON, 
-#             create_pcd_npz=createNpzs
-#             )
-        
-#         await websocket.send(CreateStateUpdate("Finished"))
-
-#         time.sleep(2)
-
-#         await websocket.send(CreateStateUpdate("Idle"))
-
+        return proxy
 
 
 async def connect_to_server():
     calibrationHooks = WebSocketHooks()
-    calibrator = Calibrator()
+    calibrator = SocketCalibrator()
     url: str = f"ws://{webSocketAdress}:{webSocketPort}"
     async with websockets.connect(url) as websocket:
+        calibrator.webSocket = websocket
         async def OnTriggerCalibration(body:object):
 
-            subCount = body["SubCount"]
-            markerLength = body["MarkerLength"]
-            subPath= body["SubPath"]
-            createPCDJSON = body["CreateJSON"]
+            await calibrator.main(baseProxy=body)            
 
-            useCharuco = body["UseCharuco"]
-            icpItteration = body["IcpItteration"]
-            amountPcdFrames = body["AmountPcdFrames"]
-            pcdJustCenter = body["PcdJustCenter"]
-            createNpzs = body["CreateNpzs"]
 
-            await calibrator.main(websocket=websocket,subCount=subCount,markerLength=markerLength,subPath=subPath,createPCDJSON=createPCDJSON,useCharuco=useCharuco,icpItteration=icpItteration,amountPcdFrames=amountPcdFrames,pcdJustCenter=pcdJustCenter,createNpzs=createNpzs)
-
+           
         calibrationHooks.SubscribeHookListener(hook="ON_TRIGGER_CALIBRATION",listener=OnTriggerCalibration)
 
         initCalibrator: ReceivedEvent = ReceivedEvent("INIT_CALIBRATOR")
